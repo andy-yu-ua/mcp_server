f89e4cca80015b8b541086d409bc7f15a3b07662
Andy Yu <90707366+andy-yu-ua@users.noreply.github.com>
2025-09-30 14:20:45 +0000

    test for fastmcp deployment

--- weather_mcp_tools.py
+++ weather_mcp_tools.py
@@ -1,37 +1,11 @@
-import uvicorn
-import yfinance as yf
-from mcp.server.fastmcp import FastMCP
-from starlette.applications import Starlette
-from starlette.routing import Mount
+from fastmcp import FastMCP
 
 mcp = FastMCP("weather-server")
 
-
-@mcp.tool()
-def get_current_stock_price(ticker):
-    """Get Current Stock Price for a given Ticker Symbol"""
-    return {"currency": "USD", "value": yf.Ticker(ticker).info["open"]}
-
-
-@mcp.tool()
-def get_historical_stock_splits(ticker):
-    """Get list of historical stock splits"""
-    history = []
-    for timestamp, ratio in yf.Ticker(ticker).splits.to_dict().items():
-        history.append(
-            {
-                "date": timestamp.strftime("%A, %B %d, %Y") + f", {timestamp.tzname()}",
-                "ratio": ratio,
-            }
-        )
-    return {"total": len(history), "history": history}
-
-
-app = Starlette(
-    routes=[
-        Mount("/", app=mcp.sse_app()),
-    ]
-)
+@mcp.tool
+def get_weather(city: str) -> str:
+    """Get Current Weather for a given city"""
+    return f"The weather in {city} is snowy."
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    mcp.run(transport="http", port=8000)
